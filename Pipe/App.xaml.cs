f51721df6949cc34771a35a93c088a9b647e05ee
LahkLeKey <LahkLeKey@gmail.com>
2022-10-23 18:48:02 -0500

    Generate Project Files

---
+++ Pipe/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Pipe;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ Pipe/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Pipe;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
